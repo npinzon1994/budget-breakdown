1044657af387878daeb54dcb8b2ccf5745519e17
Nikki Pinzon <npinzon1994@gmail.com>
2024-05-29 16:21:17 -0400

    updated index js file to index tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./redux-store";
 import { PersistGate } from "redux-persist/integration/react";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
